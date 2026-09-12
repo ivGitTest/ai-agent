4897796283ede3867554baaa2f609334b0fdd457
Ivan Rybakov <glowinhaze@gmail.com>
2026-09-12 23:29:02 +0300

    add template

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from langchain_openrouter import ChatOpenRouter
 from environs import Env
-from langchain_core.prompts import ChatMessagePromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
+from datetime import date
 
 env = Env()
 env.read_env()
 
 openrouter_api_key = env.str("openrouter-api-key")
+
+current_date = date.today()
 
 llm = ChatOpenRouter(
     model = "openrouter/free",
@@ -13,16 +16,21 @@ llm = ChatOpenRouter(
     api_key = openrouter_api_key
 )
 
-
 user_message = input("User: ")
 
-
 messages = [
-    ("system", "ты ии-помощник, отвечай кратко, 3-5 предложений, по делу, если не знаешь - уточни запрос, спроси человека"),
+    ("system", "Сегодня {current_date}. Ты ии-помощник, отвечай кратко, 3-5 предложений, по делу, если не знаешь - уточни запрос, спроси человека"),
     ("human", user_message)
 ]
 
-agent_response = llm.invoke(messages).content
+prompt_template = ChatPromptTemplate(messages)
+prompt_value = prompt_template.invoke(
+    {
+        "current_date": current_date
+    }
+)
+
+agent_response = llm.invoke(prompt_value).content
 
 print("AI: " + agent_response)
 
