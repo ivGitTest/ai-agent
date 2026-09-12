64929c74091a5d18877fa4674403c0681ec8a944
Ivan Rybakov <glowinhaze@gmail.com>
2026-09-12 22:38:21 +0300

    add system message

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from langchain_openrouter import ChatOpenRouter
+from environs import Env
+from langchain_core.prompts import ChatMessagePromptTemplate
+
+env = Env()
+env.read_env()
+
+api_key = env.str("api-key")
 
 llm = ChatOpenRouter(
     model = "openrouter/free",
-    temperaure = 0.7
+    temperature = 0.7,
+    api_key = api_key
 )
+
 
 user_message = input("User: ")
 
-#agent_response = llm.invoke(user_message).content
 
-print(user_message)
+messages = [
+    ("system", "ты ии-помощник, отвечай кратко, 3-5 предложений, по делу, если не знаешь - уточни запрос, спроси человека"),
+    ("human", user_message)
+]
+
+agent_response = llm.invoke(messages).content
+
+print("AI: " + agent_response)
+
